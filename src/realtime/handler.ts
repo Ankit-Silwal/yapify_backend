dedfd5b7439ccd8ecab7f7e0e48ba359594a68c5
GubbinLubbin <silwalankit27@gmail.com>
2026-02-05 23:02:55 +0530

    Fixed handler

--- src/realtime/handler.ts
+++ src/realtime/handler.ts
@@ -1,17 +1,61 @@
 import { Server, Socket } from "socket.io";
 import socketAuth from "./auth.ts";
+import { createMessage } from "../modules/users/messageService.ts";
+import pool from "../config/db.ts";
 
 export default function registerHandlers(io: Server)
 {
   io.use(socketAuth);
-  io.on("connection", (socket: Socket) =>
+
+  io.on("connection", async (socket: Socket) =>
   {
     const userId = socket.data.user.id;
-    console.log("User connected:", userId);
-    socket.join(userId);
+    console.log("connected:", userId);
+
+    const result = await pool.query(`
+      SELECT conversation_id
+      FROM conversation_participants
+      WHERE user_id=$1
+      AND deleted_at IS NULL
+    `,[userId]);
+
+    result.rows.forEach(row =>
+    {
+      socket.join(row.conversation_id);
+    });
+
+    socket.on("message:send", async (payload) =>
+    {
+      try
+      {
+        const message = await createMessage(
+          userId,
+          payload.conversationId,
+          payload.content,
+          payload.messageType
+        );
+
+        // 1️ACK sender
+        socket.emit("message:ack", {
+          tempId: payload.tempId,
+          messageId: message.id
+        });
+
+        // 2 SEND TO ALL IN CONVERSATION
+        io.to(payload.conversationId).emit("message:new", message);
+      }
+      catch(err:any)
+      {
+        socket.emit("message:error", {
+          tempId: payload.tempId,
+          message: err.message
+        });
+      }
+    });
+
     socket.on("disconnect", () =>
     {
-      console.log("User offline:", userId);
+      console.log("offline:", userId);
     });
   });
 }
